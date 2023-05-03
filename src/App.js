c829de8ae2ac254a2cd16ff91eb136c74212231a
Zachary Lian <zachary.lian@gmail.com>
2023-05-03 16:27:07 +0800

    Router Issue

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/navbar';
+import Notes from './pages/notes';
+import Create from './pages/create';
 
 function App() {
+  const title = "Welcome!";
+  // cannot output objects and boolean
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <h1>{ title }</h1>
+
+      <Router>
+        <Routes>
+          <Route exact path="/">
+            <Notes /> 
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
---
+++ src/components/navbar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Menu</h1>
+            <div className="links">
+                <a href="/">Home</a> | 
+                <a href="/">About</a> | 
+                <a href="/">Projects</a> | 
+                <a href="/">Contact</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
