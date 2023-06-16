b5f5596b31e1a539886c7703919994e4ce2f6581
Zachary Lian <zachary.lian@gmail.com>
2023-06-16 17:40:10 +0800

    Collapse Button

--- src/pages/experience/projects.js
+++ src/pages/experience/projects.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './projects.scss'
 import projList from './projects/projectList'
+import ProjCard from './projects/projCard';
 
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
@@ -9,11 +10,16 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Grid';
+import Collapse from '@mui/material/Collapse';
+import KeyBoardArrowUp from '@mui/icons-material/KeyboardArrowUp';
+import KeyBoardArrowDown from '@mui/icons-material/KeyboardArrowDown';
 
-const projects = () => {
+const Projects = () => {
+  const [open, setOpen] = React.useState(-1);
+
   const projs = projList.map((proj, index) => (
     <Grid item xs={12} sm={6} md={4} key={index}> 
-      <Card sx={{ maxWidth: 390, borderRadius: "5%" }}>
+      <Card sx={{ maxWidth: 390, borderRadius: "5%", padding: "10px" }}>
           <CardMedia
             component="img"
             alt={proj.name}
@@ -24,12 +30,24 @@ const projects = () => {
             <Typography gutterBottom variant="h5" color="text.secondary" fontWeight={"bold"}>
               {proj.title}
             </Typography>
-            <Typography variant="h6" color="text.secondary">
-              {proj.subtitle}
+            <Typography variant="h8" color="text.secondary" fontWeight={"bold"}>
+              {proj.date}
+            </Typography>
+            <Typography variant="body1" color="text.secondary" >
+              <b>Skills:</b> {proj.skills}
             </Typography>
           </CardContent>
           <CardActions>
-            <Button size="small">Learn More</Button>
+            <Button size="small" sx={{marginX: "auto"}} onClick={() => setOpen(open === index ? -1 : index)}>
+              {open === index ? (
+                <KeyBoardArrowUp />
+              ) : (
+                <KeyBoardArrowDown />
+              )}
+              <Collapse in={open === index} timeout="auto" unmountOnExit>
+                <ProjCard />
+              </Collapse>
+            </Button>
           </CardActions> 
       </Card>
     </Grid>
@@ -41,4 +59,4 @@ const projects = () => {
   )
 }
 
-export default projects
+export default Projects
---
+++ src/pages/experience/projects/projCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import projList from './projects/projectList'
+
+import Card from '@mui/material/Card';
+
+const ProjCard = () => {
+  return (
+    <Card sx={{ maxWidth: 390, borderRadius: "5%", padding: "10px" }}>
+        asdfasdfasdf
+    </Card>
+  )
+}
+
+export default ProjCard
