0db75e299030b2fa9476d351b82a67f5c341d4cb
Zachary Lian <zachary.lian@gmail.com>
2023-06-21 17:25:14 +0800

    About Edits

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -1,7 +1,7 @@
 import "./index.scss";
 import { IconContext } from "react-icons";
 import { IoLogoPython, IoLogoHtml5, IoLogoCss3 } from "react-icons/io";
-import { IoLogoVue, IoLogoJavascript, IoLogoTableau } from "react-icons/io5";
+import { IoLogoVue, IoLogoJavascript, IoLogoTableau, IoLogoVercel } from "react-icons/io5";
 import { TbBrandMysql, TbBrandFigma, TbBrandGit } from "react-icons/tb";
 import { RiReactjsLine } from "react-icons/ri";
 import { FaDocker, FaBootstrap }  from "react-icons/fa";
@@ -13,7 +13,7 @@ const about = () => {
       <div className="aboutContent">
         <h1>About Me</h1>
         <div className="adj">
-          I am a Year 2 Information Systems undergraduate majoring in Business Analytics and Digitalisation & Cloud Solutions at <b>Singapore Management University (SMU)</b>. I am an <b>adaptable</b> team player who is able to work <b>independently</b> and in a <b>fast-paced</b> environment. 
+          I am a Year 2 <b>Information Systems</b> undergraduate majoring in Business Analytics and Digitalisation & Cloud Solutions at <b>Singapore Management University (SMU)</b>. I am an <b>adaptable</b> team player who is able to work <b>independently</b> and in a <b>fast-paced</b> environment. 
           <br/>
           I am passionate about <b>data science and analytics</b>, and I am always looking for opportunities to learn and grow. During my free time, I love editing videos and eating good food! 
           <div className="techStackContent">
@@ -36,34 +36,41 @@ const about = () => {
                 </p>
               </div>
               <div className="moreIcons">
-                <IconContext.Provider value={{ size: "2em" }}>
-                  <IoLogoHtml5 />
-                  <IoLogoCss3 />
-                  <FaBootstrap />
-                  <IoLogoJavascript />
-                  <RiReactjsLine />
-                  <IoLogoVue />
-                  <SiPhp />
-                </IconContext.Provider>
+                <div className="col1">
+                  <IconContext.Provider value={{ size: "2em" }}>
+                    <IoLogoHtml5 style={{marginRight: "4px"}}/>
+                    <IoLogoCss3 style={{marginRight: "4px"}}/>
+                    <FaBootstrap style={{marginRight: "4px"}}/>
+                    <IoLogoJavascript style={{marginRight: "4px"}}/>
+                  </IconContext.Provider>
+                </div>
+                <div className="col2">
+                  <IconContext.Provider value={{ size: "2em" }}>
+                    <RiReactjsLine style={{marginRight: "4px"}}/>
+                    <IoLogoVue style={{marginRight: "4px"}}/>
+                    <SiPhp style={{marginRight: "4px"}}/>
+                    <IoLogoVercel style={{marginRight: "4px"}}/>
+                  </IconContext.Provider>
+                </div>
               </div>
             </div>
             <div className="techStackRight">
               <h2>Tools</h2>
               <div className="tools">
                 <div className="col1">
-                  <IconContext.Provider value={{ size: "2em"  }}>
-                    <TbBrandGit />
-                    <FaDocker />
-                    <SiPostman />
-                    <SiJupyter />
+                  <IconContext.Provider value={{ size: "2em" }}>
+                    <TbBrandGit style={{marginRight: "4px"}}/>
+                    <FaDocker style={{marginRight: "4px"}}/>
+                    <SiPostman style={{marginRight: "4px"}}/>
+                    <SiJira style={{marginRight: "4px"}}/>
                   </IconContext.Provider>
                 </div>
                 <div className="col2">
-                  <IconContext.Provider value={{ size: "2em"  }}>
-                    <IoLogoTableau />
-                    <TbBrandFigma />
-                    <SiAdobepremierepro />
-                    <SiJira />
+                  <IconContext.Provider value={{ size: "2em" }}>
+                    <SiJupyter style={{marginRight: "4px"}}/>
+                    <IoLogoTableau style={{marginRight: "4px"}}/>
+                    <TbBrandFigma style={{marginRight: "4px"}}/>
+                    <SiAdobepremierepro style={{marginRight: "4px"}}/>
                   </IconContext.Provider>
                 </div>
               </div>  
